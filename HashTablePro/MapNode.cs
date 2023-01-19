b42a7094013870d228b6750f62fa37e711910a96
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-19 21:21:09 +0530

    HashTable

---
+++ HashTablePro/MapNode.cs
@@ -0,0 +1,108 @@
+﻿
+
+namespace HashTablePro
+{
+    public class MyMapNode<K, V>
+    {
+        //variables defining 
+
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        //creating parameterize constructor 
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        //get and set method 
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+
+        // Gets the value at the specified key.
+        // using Generic Method to find the key value pair at the particular position
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        //to get position of array 
+        protected int GetArrayPosition(K key)
+        {
+            //for finding position use key.Hash code() % size of array
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+
+        //creating GetLinkedList method with initial position to return keyvalue pair and
+        // gets the position of items array in linked list
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        //to get frequency of values using getfrequency method
+
+        public void GetFrequency(V Values)
+        {
+            //initially our frequency is zero
+
+            int frequency = 0;
+
+            //using foreach loop to get the value in linked list
+
+            foreach (LinkedList<KeyValue<K, V>> list in items)
+            {
+                if (list == null)
+                    continue;
+
+                foreach (KeyValue<K, V> check in list)
+                {
+                    if (check.Equals(null))
+                    {
+                        continue;
+
+                    }
+                    if (check.Value.Equals(Values))
+                    {
+                        frequency++;
+
+                    }
+                }
+
+            }
+            Console.WriteLine("Frequency of \"{0}\" is : {1}", Values, frequency);
+        }
+    }
+}
---
+++ HashTablePro/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace HashTablePro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table Program");
+
+            bool check = true;
+
+
+
+            Console.WriteLine("Select Proper Option\n" +
+                "1)Get the Frequency of Words to be used\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    //creating an object of mapnode class using key value pair
+                    //key and value has both same data type
+
+                    MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+                    hash.Add("0", "To");
+                    hash.Add("1", "be");
+                    hash.Add("2", "or");
+                    hash.Add("3", "not");
+                    hash.Add("4", "To");
+                    hash.Add("5", "be");
+                    hash.GetFrequency("To");
+                    string hash5 = hash.Get("5");
+                    Console.WriteLine("5th index value: " + hash5);
+                    break;
+
+                default:
+                    Console.WriteLine("Please Enter Proper Option");
+                    break;
+            }
+        }
+    }
+}
